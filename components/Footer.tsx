c3b11ac695dd1b34360da048304a848c862889b5
carlos-bolano <carlostutos828@gmail.com>
2024-09-28 12:51:46 -0500

    styles: some improvements in footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-4 text-center">
-      <p className="text-center text-gray-500 text-sm">
+      <p className="text-center text-gray-600 text-sm drop-shadow-md">
         Created wtih{" "}
         <span className="text-red-400 text-lg animate-pulse"> ❤ </span> by{" "}
         <Link
@@ -12,10 +12,10 @@ const Footer = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <span className="text-red-400 font-semibold">Carlos Bolaño</span>
+          <span className="text-red-400 font-medium">Carlos Bolaño</span>
         </Link>
       </p>
-      <p className="text-center text-gray-500 text-sm">
+      <p className="text-center text-gray-600 text-sm drop-shadow-md">
         &copy; {new Date().getFullYear()} Moodflix. All rights reserved.
       </p>
     </footer>
