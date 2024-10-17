e64d2f369d262aa9dfd73ca7dc9cb3322ec2d943
Carlos-Bolano <carlostutos828@gmail.com>
2024-10-17 12:27:51 -0500

    refactor: Avoiding mistakes when searching for movie data

--- utils/GetMovieDetails.ts
+++ utils/GetMovieDetails.ts
@@ -1,37 +1,41 @@
 import { Genre, GENRES } from "@/constans";
 
 export const getMovieDetails = async (movieTitle: string) => {
-  const url = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=es-Es&page=1`;
+  try {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=es-Es&page=1`;
 
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_TOKEN}`,
-    },
-  };
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_TOKEN}`,
+      },
+    };
 
-  const res = await fetch(url, options);
-  const data = await res.json();
-  const details = await data.results[0];
+    const res = await fetch(url, options);
+    const data = await res.json();
+    const details = await data.results[0];
 
-  const genreNames = details.genre_ids.reduce((acc: string[], id: number) => {
-    const genre = GENRES.find((genre) => genre.id === id);
-    if (genre) {
-      acc.push(genre.name);
-    }
-    return acc;
-  }, []);
+    const genreNames = details.genre_ids.reduce((acc: string[], id: number) => {
+      const genre = GENRES.find((genre) => genre.id === id);
+      if (genre) {
+        acc.push(genre.name);
+      }
+      return acc;
+    }, []);
 
-  const movie = {
-    movieId: details.id,
-    backdroPath: `https://image.tmdb.org/t/p/w500/${details.backdrop_path}`,
-    posterPath: `https://image.tmdb.org/t/p/w500/${details.poster_path}`,
-    title: details.title,
-    vote_average: details.vote_average,
-    release_date: details.release_date,
-    genres: genreNames,
-  };
-  return movie;
+    const movie = {
+      movieId: details.id,
+      backdroPath: `https://image.tmdb.org/t/p/w500/${details.backdrop_path}`,
+      posterPath: `https://image.tmdb.org/t/p/w500/${details.poster_path}`,
+      title: details.title,
+      vote_average: details.vote_average,
+      release_date: details.release_date,
+      genres: genreNames,
+    };
+    return movie;
+  } catch (error) {
+    console.error(error);
+  }
 };
 // TODO: SET language=es-Es IN THE QUERY TO MAKE THE REQUEST
