383cadd6ad9acdcd6676bd9cdedf5c74dca17891
Carlos-Bolano <carlostutos828@gmail.com>
2024-10-31 20:56:02 -0500

    feat: add translations to the footer
    Adds translations to the footer component using `next-intl`. This improves the user experience by making the footer content more accessible to users in different languages. The `year` is now dynamically updated based on the current date.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,20 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+  const t = useTranslations("Footer");
   return (
     <footer className="py-4 text-center z-10">
       <div className="bg-[#faf7de]/40 backdrop-blur-sm max-w-max mx-auto rounded-xl border-2 px-2 border-[#faf7de]">
         <p className="text-center text-gray-600 text-sm drop-shadow-md">
-          Created wtih <span className="text-red-400 text-lg animate-pulse"> ❤ </span> by{" "}
+          {t("createdWith")} <span className="text-red-400 text-lg animate-pulse"> ❤ </span> {t("by")}
           <Link href="https://carlos-bolano.vercel.app/" target="_blank" rel="noreferrer">
-            <span className="text-red-400 font-medium">Carlos Bolaño</span>
+            <span className="text-red-400 font-medium"> Carlos Bolaño</span>
           </Link>
         </p>
         <p className="text-center text-gray-600 text-sm drop-shadow-md">
-          &copy; {new Date().getFullYear()} Moodflix. All rights reserved.
+          &copy; {year} Moodflix. {t("allRightsReserved")}
         </p>
       </div>
     </footer>
