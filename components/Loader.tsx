a31b7ced531eb0e8a8ff2d33679b81e193eef057
Carlos-Bolano <carlostutos828@gmail.com>
2024-11-01 21:53:14 -0500

    feat: Add loading message to loader component
    Adds a more descriptive loading message to the loader component, using the `next-intl` library to provide translations for different languages. This message provides a clearer indication to the user of what is happening while the application is loading.

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { useTranslations } from "next-intl";
 
 const Loader = () => {
+  const t = useTranslations("Loader");
   return (
-    <div className="flex space-x-2 justify-center items-center">
-      <span className="sr-only">Loading...</span>
-      <div className="h-8 w-8 bg-red-400 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-      <div className="h-8 w-8 bg-red-400 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-      <div className="h-8 w-8 bg-red-400 rounded-full animate-bounce"></div>
+    <div>
+      <div className="flex space-x-2 justify-center items-center">
+        <span className="sr-only">Loading...</span>
+        <div className="h-8 w-8 bg-red-400 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="h-8 w-8 bg-red-400 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="h-8 w-8 bg-red-400 rounded-full animate-bounce"></div>
+      </div>
+      <span className="text-center text-sm text-slate-600 drop-shadow-sm">{t("loading")}</span>
     </div>
   );
 };
