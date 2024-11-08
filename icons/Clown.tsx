f4121ec32f210ba95c8e3d25277325e1e45e1e9c
Carlos-Bolano <carlostutos828@gmail.com>
2024-11-08 10:53:56 -0500

    feat: Add Clown emoji icon
    Adds a new `Clown` component that displays the clown emoji as an image. This will be used in some buttons

---
+++ icons/Clown.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const Clown = () => {
+  return (
+    <Image
+      src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Clown%20Face.png"
+      alt="Clown Face emoji"
+      width={25}
+      height={25}
+    />
+  );
+};
+
+export default Clown;
