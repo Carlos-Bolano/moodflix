46690a99c0a51229eeb425c7126c90a59167ff6d
Carlos-Bolano <carlostutos828@gmail.com>
2024-11-08 10:54:24 -0500

    feat: Add Clapper Board emoji icon
    Adds a new component to display the Clapper Board emoji icon. This component uses an external image source to display the emoji

---
+++ icons/Clapper.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const Clapper = () => {
+  return (
+    <Image
+      src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Clapper%20Board.png"
+      alt="Clapper Board emoji"
+      width={25}
+      height={25}
+    />
+  );
+};
+
+export default Clapper;
